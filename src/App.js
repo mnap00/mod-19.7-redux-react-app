46f913a0ef44995ec7150fd3411070ebfc907a06
mnap <learn@10g.pl>
2018-05-15 20:43:27 +0200

    Add App component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
+const App = () => {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div className='App'>
+            CommentsList here
+        </div>
     );
-  }
-}
+};
 
 export default App;
